98dec2c0cbbb157b15e6594881ee2b4fdfde3751
ihjas-ahammed <ihjaskallingal@gmail.com>
2023-07-10 07:35:52 +0530

    Changes

--- app/src/main/java/in/ghsvkd/bellsystem/list/ConfigurationListAdapter.java
+++ app/src/main/java/in/ghsvkd/bellsystem/list/ConfigurationListAdapter.java
@@ -2,23 +2,28 @@ package in.ghsvkd.bellsystem.list;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.strictmode.ImplicitDirectBootViolation;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.ItemTouchUIUtil;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.RecyclerView.ViewHolder;
 import com.google.android.material.snackbar.Snackbar;
+import in.ghsvkd.bellsystem.R;
+import in.ghsvkd.bellsystem.databinding.AlertNewConfigurationBinding;
 import in.ghsvkd.bellsystem.databinding.ListItemConfigurationBinding;
 import in.ghsvkd.bellsystem.obj.Configuration;
 import in.ghsvkd.bellsystem.ui.ConfigurationFragment;
-import in.ghsvkd.bellsystem.R;
 import java.io.File;
 import java.time.chrono.Era;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ConfigurationListAdapter
@@ -116,6 +121,62 @@ public class ConfigurationListAdapter
   public void onBindViewHolder(final ConfigurationListAdapter.ViewHolder arg0, final int arg1) {
     arg0.binding.textConfiguration.setText(configurationList.get(arg1).label);
     arg0.binding.getRoot().setOnClickListener(openEvent(arg1));
+    View v = arg0.binding.getRoot();
+    v.setOnLongClickListener(new View.OnLongClickListener(){public boolean onLongClick(View v){
+        final android.widget.TextView tv = arg0.binding.textConfiguration;
+        final Configuration cfg = configurationList.get(arg1);
+        AlertDialog.Builder alert = new AlertDialog.Builder(v.getContext());
+        alert.setItems(new CharSequence[]{"Duplicate","Rename"},new DialogInterface.OnClickListener(){public void onClick(DialogInterface di,int i){
+            if(i == 1){
+                AlertDialog.Builder adb = new AlertDialog.Builder(v.getContext());
+                final AlertNewConfigurationBinding ancb = AlertNewConfigurationBinding.inflate(LayoutInflater.from(v.getContext()));
+                adb.setTitle("Set label");
+                                    
+                adb.setView(ancb.getRoot());
+                ancb.inputConfig.setText(cfg.label);                    
+                adb.setPositiveButton("CREATE",new DialogInterface.OnClickListener(){public void onClick(DialogInterface di,int id){
+                    if(!ancb.inputConfig.getText().toString().isEmpty()){
+                        File oldf = new File(v.getContext().getFilesDir(),"Configs/"+cfg.label+".obj");                            
+                        oldf.delete();
+                                                    
+                        cfg.label = ancb.inputConfig.getText().toString();
+                        try{
+                           cfg.writeToRoot(new File(v.getContext().getFilesDir(),"Configs"));                            
+                        }catch(Throwable e){
+                            
+                        }
+                        ConfigurationListAdapter.lastUsed.notifyItemChanged(arg1);                                                
+                    }
+                }});
+                adb.create().show();        
+                
+            }else if(i == 0){
+                Configuration dupe = new Configuration(cfg.label);
+                dupe.active = cfg.active;
+                dupe.listLength = cfg.listLength;
+                dupe.label = cfg.label+" (Copy)";
+                                    
+                for(Configuration.TimeData t:cfg.timeDataList){
+                    Configuration.TimeData n = new Configuration.TimeData();
+                    n.active = t.active;
+                    n.day = t.day;
+                    n.sound = t.sound;
+                    n.time = t.time;
+                    dupe.timeDataList.add(n);                                                                                                      
+                }
+                try{
+                    dupe.writeToRoot(new File(v.getContext().getFilesDir(),"Configs"));
+                }catch(Throwable e){
+                    
+                }                    
+                configurationList.add(dupe);
+                notifyDataSetChanged();                                        
+            }
+        }});
+        alert.create().show();
+                                
+        return true;
+    }});     
     if(!configurationList.get(arg1).active)   arg0.binding.buttonActiveItem.setText(""); 
     else arg0.binding.buttonActiveItem.setText("{zmdi-check}");    
     arg0.binding.buttonActiveItem.setOnClickListener(new View.OnClickListener(){public void onClick(View v){
